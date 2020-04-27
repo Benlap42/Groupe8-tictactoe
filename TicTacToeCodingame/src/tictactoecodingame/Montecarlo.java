2c4f535de8ef51236376b5c53f97f380b10c9f4a
Unknown <Condeines4@gmail.com>
2020-04-27 18:00:48 +0200

    Version améliorer

--- TicTacToeCodingame/src/tictactoecodingame/Montecarlo.java
+++ TicTacToeCodingame/src/tictactoecodingame/Montecarlo.java
@@ -26,12 +26,47 @@ public class Montecarlo extends AlgoRecherche  {
     
     public Coup meilleurCoup(Plateau _plateau, Joueur _joueur, boolean _ponder) {
         
+        ArrayList<Coup> coups = _plateau.getListeCoups(_joueur);
+
         Coup dernier = _plateau.getDernierCoup();
         if (dernier!=null){
             adv=joue.fils(joue,dernier);
+
+             _plateau.sauvegardePosition(0);
+        
+            for (int i=0; i<coups.size();i++){
+                _plateau.joueCoup(coups.get(i));
+                _plateau.sauvegardePosition(1);
+                if (_plateau.partieTerminee()){
+                    if(_plateau.partieGagnee()){
+                        _plateau.restaurePosition(0);
+                        joue=adv.fils(adv, coups.get(i));
+                        joue.booster(adv, coups.get(i));
+                        return joue.coupP;
+                    }
+                }else {
+                    Joueur A = dernier.getJeton().getJoueur();
+                    ArrayList<Coup> coups2 = _plateau.getListeCoups(A);
+                    for (int j=0; j<coups2.size();j++){
+                        _plateau.joueCoup(coups2.get(j));
+                        if (_plateau.partieTerminee()){
+                            if (_plateau.partieGagnee()){
+                                Coup c = coups2.get(j);
+                                c.getJeton().setJoueur(_joueur);
+                                _plateau.restaurePosition(0);
+                                joue=adv.fils(adv, c);
+                                joue.booster(adv, c);
+                                return joue.coupP;
+                            }
+                        }
+                        _plateau.restaurePosition(1);
+                    }
+                }
+                _plateau.restaurePosition(0);
+            }
+
         }
         
-        ArrayList<Coup> coups = _plateau.getListeCoups(_joueur);
         joue=adv.MeilleurNoeud(adv, C,coups);
         if (joue!=null){
             return joue.coupP;
